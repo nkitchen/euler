504d0365913651b0902509ec2f1e28056b775b07
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-01-23 10:49:13 -0800

    Reimplemented using Joda Time.

--- p19.clj
+++ p19.clj
@@ -2,32 +2,26 @@
 ;;; century (1 Jan 1901 to 31 Dec 2000)?
 
 (ns euler
-  (:import java.util.Calendar))
+  (:import (org.joda.time DateTimeConstants LocalDate)))
 
-(defn inc-month [cal]
-  (doto (.clone cal)
-    (.add Calendar/MONTH 1)))
+;(def df (new java.text.SimpleDateFormat "yyyy-MM-dd EEE"))
+;
+;(defmethod print-method Calendar [cal, #^java.io.Writer w]
+;  (.write w "#<")
+;  (.write w (.format df (.getTime cal)))
+;  (.write w ">"))
 
-(def df (new java.text.SimpleDateFormat "yyyy-MM-dd EEE"))
+(def start (LocalDate. 1901 1 1))
+(def end (LocalDate. 2000 12 31))
 
-(defmethod print-method Calendar [cal, #^java.io.Writer w]
-  (.write w "#<")
-  (.write w (.format df (.getTime cal)))
-  (.write w ">"))
-
-(def start (doto (Calendar/getInstance)
-             (.set Calendar/DAY_OF_MONTH 1)
-             (.set Calendar/MONTH Calendar/JANUARY)
-             (.set Calendar/YEAR 1901)))
-(def end (doto (Calendar/getInstance)
-           (.set Calendar/DAY_OF_MONTH 31)
-           (.set Calendar/MONTH Calendar/DECEMBER)
-           (.set Calendar/YEAR 2000)))
+(defn inc-month [#^LocalDate date]
+  (.plusMonths date 1))
 
 (def month-firsts (take-while #(neg? (compare % end))
                               (iterate inc-month start)))
 
-(defn sunday? [cal]
-  (= (.get cal Calendar/DAY_OF_WEEK) Calendar/SUNDAY))
+(defn sunday? [#^LocalDate date]
+  (when-let [p (= (.getDayOfWeek date) DateTimeConstants/SUNDAY)]
+    (do (prn date) p)))
 
 (prn (count (filter sunday? month-firsts)))
