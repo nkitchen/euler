f09fccbe1d03c8d7fdb6cf8726eb391de9a0a9b5
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-07-07 14:29:23 -0700

    Solved problem 59.

--- p59.clj
+++ p59.clj
@@ -2,65 +2,80 @@
   (:use euler.core
         [clojure.contrib.duck-streams :only [reader writer]]
         [clojure.contrib.math]
-        [clojure.contrib.repl-ln]))
+        [clojure.contrib.repl-ln]
+        [clojure.contrib.str-utils]))
 
-(defn decode [key bytes]
-  (map #(bit-xor % key) bytes))
+(defn decode [[k1 k2 k3 :as key] text]
+  (apply str (map #(char (bit-xor (byte (int %1))
+                                  (byte (int %2))))
+                  (cycle key)
+                  text)))
 
-(defn decode-str [key bytes]
-  (apply str (map char (decode key bytes))))
+(def english-words
+  (set (map #(.toLowerCase #^String %)
+            (re-split #"\n" (slurp "/usr/share/dict/words")))))
 
-(defn freqs [xs]
-  (apply merge-with + {} (for [x xs] {x 1})))
+(defn english-count [text]
+  (pipe text
+        (re-seq #"[\w']+")
+        (map #(.toLowerCase %))
+        (filter english-words)
+        count))
 
-(defn distro [fs]
-  (let [z (apply + (vals fs))]
-    (into {} (for [[x n] fs] [x (/ n z)]))))
-
-(defn tvd
-  "Total variation distance"
-  [p q]
-  (* 1/2 (sum (for [x (set (concat (keys p) (keys q)))]
-                (abs (- (p x 0) (q x 0)))))))
-(def tvd (memoize tvd))
-
-(def english-distro
-;  (with-open [#^java.io.BufferedReader r (reader "/tmp/corpus")]
-;    (loop [f {}]
-;      (let [c (int (.read r))]
-;        (cond
-;          (= c -1) (distro f)
-;          (Character/isLetter c) (let [u (Character/toUpperCase c)]
-;                                   (recur (merge-with + f {u 1})))
-;          :else (recur f))))))
-  (with-open [r (new java.io.PushbackReader (reader "english-distro.clj"))]
-    (read r)))
-;(with-open [w (writer "english-distro.clj")]
-;  (.println w english-distro))
-
-;(prn (#(for [[c n] %] [(char c) n]) (sort-by second (into [] english-distro))))
-
-(def cipher
+(def cipher-text
   (with-open [r (new java.io.PushbackReader (reader "cipher1.clj"))]
     (read r)))
 
-(def as (map (fn [i] (apply concat (partition 1 3 (drop i cipher))))
-             (range 3)))
+(def *top-count* 3)
 
-(defn decodings [bytes]
-  (into {} (for [key (range (int \a) (inc (int \z)))]
-             {key (decode key bytes)})))
+(def lowercase-letters (range (int \a) (inc (int \z))))
 
-(defn tvd-to-english [bytes]
-  (tvd (distro (freqs (map #(Character/toUpperCase (char %)) bytes)))
-       english-distro))
-(def tvd-to-english (memoize tvd-to-english))
+(def all-keys (for [k1 lowercase-letters
+                    k2 lowercase-letters
+                    k3 lowercase-letters]
+                [k1 k2 k3]))
 
-(def best-decodings
-  (map (fn [bytes]
-         (let [ds (into [] (decodings bytes))]
-           (last (sort-by #(tvd-to-english (second %)) ds))))
-       as))
+(def ans (reduce + (map int (decode "god" cipher-text))))
+(prn ans)
+;;(def top-decodings
+;;  (loop [rkeys all-keys
+;;         best (sorted-map-by #(compare (:score %1) (:score %2)))]
+;;    (if (seq rkeys)
+;;      (let [key (first rkeys)
+;;            decoded (decode key cipher-text)
+;;            score (english-count decoded)
+;;            item {:key key :decoded decoded :score score}]
+;;        (cond
+;;          (< (count best) *top-count*)
+;;            (recur (next rkeys) (assoc best item 1))
+;;          (> score (:score (ffirst best)))
+;;            (recur (next rkeys)
+;;                   (-> best
+;;                     (dissoc (ffirst best))
+;;                     (assoc item 1)))
+;;          :else (recur (next rkeys) best)))
+;;      best)))
+;;
+;;(prn top-decodings)
 
-(prn (apply str (apply interleave best-decodings)))
-;(repl)
+;
+;(def as (map (fn [i] (apply concat (partition 1 3 (drop i cipher))))
+;             (range 3)))
+;
+;(defn decodings [bytes]
+;  (into {} (for [key (range (int \a) (inc (int \z)))]
+;             {key (decode key bytes)})))
+;
+;(defn tvd-to-english [bytes]
+;  (tvd (distro (freqs (map #(Character/toUpperCase (char %)) bytes)))
+;       english-distro))
+;(def tvd-to-english (memoize tvd-to-english))
+;
+;(def best-decodings
+;  (map (fn [bytes]
+;         (let [ds (into [] (decodings bytes))]
+;           (last (sort-by #(tvd-to-english (second %)) ds))))
+;       as))
+;
+;(prn (apply str (apply interleave best-decodings)))
+;;(repl)
