7b30a8543d4cc2d4611f9229ac2ca3331888b327
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-08-14 10:44:17 -0700

    Generalized solution.

--- p62.clj
+++ p62.clj
@@ -4,16 +4,23 @@
 
 (set! *warn-on-reflection* true)
 
-;; Happens to work for 5 permutations, but fails for 6, because a greater cube
-;; with five other permutations, including a lesser one, may still be found.
+(def *goal* 6)
+
 (def ans
   (loop [n 100
-         permutations {}]
+         permutations {}
+         candidates (sorted-set)]
     (let [c (* n n n)
           p (sort (digits c))
-          qs (get permutations p [])]
-      (if (= (count qs) 4)
-        (first qs)
-        (recur (inc n)
-               (assoc permutations p (conj qs c)))))))
+          qs (get permutations p [])
+          pn (assoc permutations p (conj qs c))]
+      (cond
+        (= (count qs) *goal*)
+          (first candidates)
+        (= (count qs) (dec *goal*))
+          (recur (inc n)
+                 pn
+                 (conj candidates (first qs)))
+        :else
+          (recur (inc n) pn candidates)))))
 (repl)
