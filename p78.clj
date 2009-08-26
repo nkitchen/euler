a0c2f3b4d1cc638c1c5d1d0c0127f4862a2f1ae4
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-08-26 16:43:59 -0700

    Fixed problem 78 (also old code using pentagonal numbers).

--- p78.clj
+++ p78.clj
@@ -4,24 +4,47 @@
 
 (set! *warn-on-reflection* true)
 
-(defn partition-count
-  "Returns the number of ways in which n can be written as a sum of positive
-  integers."
-  ([n max-part]
-   (if (= n 1)
-     0
-     (sum (for [k (range 1 (inc max-part))
-                :while (< k n)]
-            (let [d (- n k)
-                  pc (partition-count d k)]
-              (if (and (<= d max-part)
-                       (<= d k))
-                (inc pc)
-                pc))))))
-  ([n] (partition-count n (dec n))))
-(def partition-count (memoize partition-count))
+(def gen-pent-nums
+  (map #(* 1/2 % (- (* 3 %) 1))
+       (for [i (iterate inc 1)
+             s [1 -1]]
+         (* s i))))
 
 (def ans
-  (first (filter #(== 0 (rem (inc (partition-count %)) 1000000))
-                 (iterate inc 10))))
+  (loop [n 2
+         pmemo {0 1, 1 1}]
+    (let [p (sum (for [[s k] (map vector
+                                  (cycle [1 1 -1 -1])
+                                  (map #(- n %) gen-pent-nums))
+                       :while (>= k 0)]
+                   (* s (pmemo k))))
+          q (rem p 1000000)]
+      (if (zero? q)
+        n
+        (recur (inc n) (assoc pmemo n q))))))
+
+;(defn partition-count
+;  "Returns the number of ways in which n can be written as a sum of positive
+;  integers."
+;  [n]
+;  (cond
+;    (neg? n) 0
+;    (<= n 1) 1
+;    :else
+;    (rem
+;      (sum (for [[s t] (map vector
+;                            (cycle [1 1 -1 -1])
+;                            (map #(partition-count (- n %)) gen-pent-nums))
+;                 :while (pos? t)]
+;             (rem (* s t) 1000000)))
+;      1000000)))
+;(def partition-count (memoize partition-count))
+
+;(def ans
+;  (first (filter #(== 0 (rem (inc (partition-count %)) 1000000))
+;                 (map #(if (= 0 (rem % 1000000))
+;                         (do (prn %) %)
+;                         %)
+;                        (iterate inc 10))))
+;  )
 (repl)
