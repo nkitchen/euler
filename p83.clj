1caffb62cc5164853e1154a2ff49834af6d702c3
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-09-04 12:07:26 -0700

    Solved problem 83.

--- p83.clj
+++ p83.clj
@@ -2,7 +2,7 @@
   (:use euler.core
         clojure.contrib.duck-streams
         clojure.contrib.repl-ln)
-  (:import java.util.PriorityQueue))
+  (:import clojure.lang.PersistentQueue))
 
 (set! *warn-on-reflection* true)
 
@@ -10,32 +10,33 @@
   ; A* search from [0 0] to [m n]
   (let [m (count mat)
         n (count (mat 0))
-        u (reduce min (map #(reduce min %) mat))
-        dist (fn [i j] (+ (Math/abs (double (- m 1 i)))
-                          (Math/abs (double (- n 1 j)))))
-        queue (PriorityQueue.)]
-    (.add queue (with-meta [(get-in mat [0 0]) [0 0]]
-                           {:path #{[0 0]}}))
-    (loop []
-      (let [entry (.poll queue)
-            s (first entry)
-            [i j :as ij] (second entry)
-            {path :path} (meta entry)]
-        (if (and (== i (dec m))
-                 (== j (dec n)))
-          s
-          (do 
-            (doseq [p [0 1]
-                    f [inc dec]
-                    :let [[k l :as kl] (update-in ij [p] f)]
-                    :when (<= 0 k (dec m))
-                    :when (<= 0 l (dec n))
-                    :when (not (path kl))
-                    :let [e (* (dist k l) u)
-                          c (+ s e (get-in mat [k l]))]]
-              (.add queue (with-meta [c kl]
-                                     {:path (conj path kl)})))
-            (recur)))))))
+        neighbors (fn [ij]
+                    (for [p [0 1]
+                          f [inc dec]
+                          :let [[k l :as kl] (update-in ij [p] f)]
+                          :when (<= 0 k (dec m))
+                          :when (<= 0 l (dec n))]
+                      kl))]
+    (loop [queue (conj (PersistentQueue/EMPTY) [0 0])
+           best (vec (repeat m (vec (repeat n nil))))]
+      (if (empty? queue)
+        (get-in best [(dec m) (dec n)])
+        (let [[i j :as ij] (peek queue)
+              c (get-in mat ij)
+              sold (get-in best ij)
+              adj-sums (for [kl (neighbors ij)
+                             :let [s (get-in best kl)]
+                             :when s]
+                         s)
+              snew (cond
+                     (seq adj-sums) (+ c (reduce min adj-sums))
+                     sold sold
+                     :else c)]
+          (if (or (nil? sold) (< snew sold))
+            (recur (apply conj (pop queue) (neighbors ij))
+                   (assoc-in best ij snew))
+            (recur (pop queue)
+                   best)))))))
 
 (def m1 [[131	673	234	103	18]
         [201	96	342	965	150]
