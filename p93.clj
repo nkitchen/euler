e3e892a1fee258b2dc47a1ca9b23bdfb162c46ac
Nathan Kitchen <nbk@eecs.berkeley.edu>
2009-12-31 17:19:07 -0800

    Completed code for Problem 93.

--- p93.clj
+++ p93.clj
@@ -6,17 +6,40 @@
 
 (set! *warn-on-reflection* true)
 
+(defn sdiv [a b]
+  (if (zero? b)
+    Double/POSITIVE_INFINITY
+    (/ a b)))
+
 (defn targets
   "Returns all possible values obtained by combining VALUES with OPS in order
   (that is, the values obtained by all different groupings)."
-  [ops values]
-  (assert (= (count ops) (dec (count values))))
-  (if (empty? ops)
-    (list (values 0))
-    (for [r (range (count ops))
-          left (targets (subvec ops 0 r) (subvec values 0 (inc r)))
-          right (targets (subvec ops (inc r)) (subvec values (inc r)))]
-      ((ops r) left right))))
+  ([ops values]
+   (assert (= (count ops) (dec (count values))))
+   (if (empty? ops)
+     (list (values 0))
+     (for [r (range (count ops))
+           left (targets (subvec ops 0 r) (subvec values 0 (inc r)))
+           right (targets (subvec ops (inc r)) (subvec values (inc r)))]
+       ((ops r) left right))))
+  ([digits]
+   (->> (for [ops (selections [+ - * sdiv] (dec (count digits)))
+              values (permutations digits)]
+          (targets (vec ops) (vec values)))
+        (apply concat)
+        (filter #(= % (int %)))
+        (filter pos?))))
 
+(defn run-length [digits]
+  (let [hits (reduce conj (sorted-set) (targets digits))
+        [a b] (first (->> (partition 2 1 hits)
+                          (remove (fn [[a b]] (= a (dec b))))))]
+    (or a -1)))
+
+(def ans
+  (apply max-key
+         second
+         (for [digits (combinations (range 0 10) 4)]
+           [digits (run-length digits)])))
 (repl)
 
