0388d71e5b7698147cd717f45586a7ed5b725769
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-01-16 11:15:27 -0800

    Attempted optimization.

--- p94.clj
+++ p94.clj
@@ -1,11 +1,13 @@
 (ns euler.p94
+  (:gen-class)
   (:use euler.core
         clojure.contrib.repl-ln
         clojure.contrib.repl-utils))
 
 (set! *warn-on-reflection* true)
 
-(def ans
+(defn -main []
+  (let [ans
 ;  (sum
 ;    (for [s (iterate #(+ % 2) 3)
 ;          off [-1 1]
@@ -15,24 +17,22 @@
 ;                h (long (Math/sqrt d))]
 ;          :when (= (* h h) d)]
 ;      (+ s s t))))
-  (loop [s 3
-         off -1
-         total 0]
+  (loop [s (long 3)
+         off (int -1)
+         total (long 0)]
     (cond
-      (> off 1) (recur (+ s 2) -1 total)
-      (> (* 3 s) 1000000001) total
+      (> off (int 1)) (recur (long (+ s (int 2))) (int -1) total)
+      (> (* (long 3) s) (long 1000000001)) total
       :else
-        (let [t (+ s off)
-              b (/ t 2)
-              d (- (* s s) (* b b))
+        (let [t (long (+ s off))
+              b (long (/ t 2))
+              d (long (- (* s s) (* b b)))
               h (long (Math/sqrt d))
-              p (+ s s t)]
-          (recur s
-                 (+ off 2)
-                 (if (= (* h h) d)
-                   (+ total p)
-                   total))))))
-
-(prn ans)
+              p (long (+ (+ s s) t))]
+          (if (== (* h h) d)
+            (recur s (int (+ off 2)) (long (+ total p)))
+            (recur s (int (+ off 2)) total)))))]
+    (prn ans)))
+(-main)
 ;(repl)
 
