226a33dd5b4fd77eb68b4bbbc151c5930ea3e23c
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-01-22 09:45:37 -0800

    Completed direct deduction for Sudoku (no guess and check)

--- p96.clj
+++ p96.clj
@@ -18,29 +18,21 @@
            (vec (for [j (range *size*)]
                   all))))))
 
-(defn read-games
-  "Reads the file and returns a sequence of known squares for each game."
-  [filename]
-  (for [lines (->> (read-lines filename)
-                   (remove #(re-find #"^Grid" %))
-                   (partition 9))]
-    (for [[i line] (indexed lines)
-          [j c] (indexed line)
-          :let [value (- (int c) (int \0))]
-          :when (not= value 0)]
-      [i j value])))
+(defn group [i j dim]
+  (if (< dim 2)
+    (for [k (range 0 *size*)]
+      (assoc [i j] dim k))
+    (for [k (range 0 3)
+          l (range 0 3)]
+      [(+ k (* 3 (quot i 3)))
+       (+ l (* 3 (quot j 3)))])))
 
-(defn neighbors [i j]
-  (->> (concat (for [k (range 0 *size*)] [i k])
-               (for [k (range 0 *size*)] [k j])
-               (for [k (range 0 3)
-                     l (range 0 3)]
-                 [(+ k (* 3 (quot i 3)))
-                  (+ l (* 3 (quot j 3)))]))
-       (remove #{[i j]})))
+(defn neighbors
+  ([i j dim] (remove #{[i j]} (group i j dim)))
+  ([i j] (apply concat (for [dim (range 3)] (neighbors i j dim)))))
 
-(defn propagate
-  "Updates the candidates in each square, given a known value at [i j]."
+(defn exclude
+  "Given known value at [i j], removes that value from neighbors."
   [board [i j value]]
   (when board
     (let [new-board (reduce #(update-in %1 %2 disj value)
@@ -53,10 +45,65 @@
                      [k l (first (get-in new-board [k l]))])]
       (if (some #(some empty? %1) new-board)
         nil
-        (reduce propagate new-board new-unit)))))
+        (reduce exclude new-board new-unit)))))
 
-; TODO: For each remaining candidate, check if it's the only square in its
-; row/column/block with that value and if so, set and propagate it.
+(defn required
+  "Returns values that remain in only one square in their groups."
+  [board]
+  (when board
+    (for [i (range 0 *size*)
+          j (range 0 *size*)
+          :let [cands (get-in board [i j])]
+          value cands
+          :when (->> (range 3)
+                     (some (fn [dim]
+                             (not-any? #((get-in board %) value)
+                                       (neighbors i j dim)))))
+          :when (not= 1 (count cands))]
+      [i j value])))
+
+(defn assign [board [i j value]]
+  (when (and board
+             ((get-in board [i j]) value))
+    (assoc-in board [i j] #{value})))
+
+(defn solve [board known]
+  (let [assigned (reduce assign board known)
+        new-board (reduce exclude assigned known)
+        inferred (required new-board)]
+    (if (seq inferred)
+      (recur (reduce assign new-board inferred)
+             inferred)
+      new-board)))
+
+(defn read-games
+  "Reads the file and returns a sequence of known squares for each game."
+  [filename]
+  (for [lines (->> (read-lines filename)
+                   (remove #(re-find #"^Grid" %))
+                   (partition 9))]
+    (for [[i line] (indexed lines)
+          [j c] (indexed line)
+          :let [value (- (int c) (int \0))]
+          :when (not= value 0)]
+      [i j value])))
+
+(defn solved? [board]
+  (when board
+    (every? #(= 1 (count %)) (for [row board, square row] square))))
+
+(defn code [board]
+  (from-digits (for [j (range 3)] (first (get-in board [0 j])))))
+
+(def games (read-games "sudoku.txt"))
+(def ans
+  (sum (for [[n game] (indexed games)]
+         (let [final (solve (empty-board) game)]
+           (if (solved? final)
+             (code final)
+             (do
+               (println "Not solved:" n)
+               0))))))
 
 (repl)
 
