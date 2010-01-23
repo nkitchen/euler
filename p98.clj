c49bdf66bbe8237e6f411ffb630771497953a9a0
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-01-23 14:39:13 -0800

    Solved Problem 98 (slowly).

--- p98.clj
+++ p98.clj
@@ -1,5 +1,6 @@
 (ns euler.p98
   (:use euler.core
+        clojure.contrib.combinatorics
         clojure.contrib.pprint
         clojure.contrib.repl-ln
         clojure.contrib.repl-utils))
@@ -21,8 +22,34 @@
     (filter (fn [[_ words]] (> (count words) 1)))
     (into {})))
 
-; Max is 9 letters, all unique.
+(defn square? [n]
+  (let [r (quot (Math/sqrt n) 1)]
+    (== (* r r) n)))
 
-;(print ans)
-(repl)
+(defn max-square
+  ([letters words]
+    (println "Finding max square for " words)
+    (let [n (count letters)]
+      (apply max
+             (for [digits (mapcat permutations (combinations (range 10) n))]
+               (max-square letters words digits)))))
+  ([letters words digits]
+    (let [n (count letters)
+          assignment (apply hash-map (interleave letters digits))
+          squares (for [w words
+                        :let [p (from-digits (map assignment w))]
+                        :when (square? p)]
+                    p)]
+      (if (and (> (count squares) 1)
+               (not-any? #(== 0 (assignment (first %))) words))
+        (apply max squares)
+        0))))
+          
+(def ans
+  (apply max
+         (for [[letters words] anagrams]
+           (max-square (distinct letters) words))))
+
+(print ans)
+;(repl)
 
