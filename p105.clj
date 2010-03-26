51a53f9e087881f8895451dd0c5e52412b52fe9a
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-03-26 09:07:26 -0700

    Unbalanced matched sequences.

--- p105.clj
+++ p105.clj
@@ -10,7 +10,7 @@
 (set! *warn-on-reflection* true)
 
 (defn balanced-seqs 
-  "Returns a sequence of all balanced sequences of length n."
+  "Returns a sequence of all balanced sequences of length N."
   [n left right]
   (cond
     (zero? n) [[]]
@@ -20,5 +20,31 @@
             s (balanced-seqs k left right)
             t (balanced-seqs (- n 1 k) left right)]
         (concat [left] s [right] t))))
+
+(defn interleavings
+  "Returns a sequence of all ways in which to interleave XS and YS."
+  ([xs ys]
+   (cond
+     (empty? xs) [ys]
+     (empty? ys) [xs]
+     :else
+       (concat (for [s (interleavings (rest xs) ys)]
+                 (cons (first xs) s))
+               (for [s (interleavings xs (rest ys))]
+                 (cons (first ys) s)))))
+  ([xs ys & others]
+   (apply concat (for [s (interleavings xs ys)
+                       zs others]
+                   (interleavings s zs)))))
+
+(defn unbalanced-matched-seqs
+  "Returns a sequence of all sequences containing N copies of LEFT  and 
+  N copies of RIGHT that start with LEFT and are not balanced."
+  [n left right]
+  (for [k (range 1 n)
+        s (balanced-seqs k left right)
+        t (interleavings (repeat (- n k) left)
+                         (repeat (- n k 1) right))]
+    (concat s [right] t)))
 
 (repl)
