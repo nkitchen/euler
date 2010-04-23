b54a4b868020431707eefa00abfd1115848afc79
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-04-22 20:11:10 -0700

    Solved Problem 104.

--- p104.clj
+++ p104.clj
@@ -5,26 +5,37 @@
         clojure.contrib.repl-utils
         clojure.contrib.seq-utils))
 
-(set! *warn-on-reflection* true)
+;(set! *warn-on-reflection* true)
 
 ; TODO: Print actual number and high and low digits to check their correctness.
 
-(loop [ah 1M
-       bh 1M
-       al 1M
-       bl 1M
+(def #^BigDecimal phi 
+  1.6180339887498948482045868343656381177203091798057628621354486M)
+
+(def #^BigDecimal invsqrt5
+  0.44721359549995793928183473374625524708812367192230514485M)
+
+(defn fibonacci [n]
+  (with-precision 20
+    (let [f (* invsqrt5 (- (.pow phi n #^java.math.MathContext *math-context*)
+                           (.pow #^BigDecimal (- 1 phi)
+                                 n
+                                 #^java.math.MathContext *math-context*)))]
+      (.toBigInteger (.round #^BigDecimal f *math-context*)))))
+
+(loop [al 1
+       bl 1
        i 1]
-  (let [ch (bigdec (with-precision 18 (+ ah bh)))
-        cl (rem (+ al bl) 1000000000)]
+  (let [cl (rem (+ al bl) 1000000000)]
     (when (zero? (rem i 1000))
         (print ".") (flush))
-    (let [dh (take 9 (digits (.unscaledValue #^java.math.BigDecimal ah)))
-          dl (digits al)]
-      (when (zero? (rem (- (.scale ch)) 1000))
-        (print "/") (flush))
-      (if (and (> ah 111111111M)
-               (pandigital? dh)
-               (pandigital? dl))
+    (when (pandigital? (digits al))
+      (prn i al))
+    (if (and (> al 111111111)
+             (pandigital? (digits al))
+             (pandigital? (take 9 (digits (fibonacci i)))))
+      (do
       (prn i)
-      (recur bh ch bl cl (inc i))))))
-;(repl)
+        (recur bl cl (inc i)))
+      (recur bl cl (inc i)))))
+(repl)
