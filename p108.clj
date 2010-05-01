b48817a223fde108c25425a261bc12e352f9300f
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-05-01 14:55:14 -0700

    Fast solution to Problem 108.

--- p108.clj
+++ p108.clj
@@ -1,6 +1,7 @@
 (ns euler.p108
   (:use euler.core
         clojure.contrib.pprint
+        clojure.contrib.lazy-seqs
         clojure.contrib.repl-ln
         clojure.contrib.repl-utils
         clojure.contrib.seq-utils
@@ -8,27 +9,44 @@
 
 (set! *warn-on-reflection* true)
 
-(defn solutions
-  "Returns each [x y] such that 1/x + 1/y = 1/n and x and y are integers."
-  [n]
-  (assert (= 0 (rem n 1)))
-  (->> (for [p (divisors n)
-             :while (<= (* p p) n)
-             :let [q (/ n p)]
-             a (drop 1 (iterate inc q))
-             :let [x (* a p)]
-             :while (<= x (+ n n))
-             :let [c (* n x)
-                   d (- x n)]
-             :when (zero? (rem c d))
-             :let [y (/ c d)]]
-         [x y])
-       (distinct)))
+(defn unfactor
+  "Converts a factor map to the product."
+  [factors]
+  (reduce (fn [m [f k]]
+            (apply * m (take k (repeat f))))
+          1
+          factors))
 
-;(def ans
-;  (first (->> (iterate inc 5)
-;              (filter #(> (let [k (count (solutions %))]
-;                            (prn [% k])
-;                            k)
-;                          1000)))))
-(repl)
+(def *init-count* 3)
+
+(defn add-factor [entry]
+  (let [[n factors] entry
+        p (first (:rest (meta entry)))]
+    (with-meta [(* n p) (conj factors [p 1])]
+               (update-in (meta entry) [:rest] rest))))
+
+(defn inc-factors [entry]
+  (let [[n factors] entry]
+    (for [[i [f m]] (indexed factors)]
+      (with-meta [(* f n) (assoc factors i [f (inc m)])]
+                 (meta entry)))))
+
+(def ans
+  (let [init-factors (->> (interleave (take *init-count* primes)
+                                      (repeat 1))
+                          (partition 2)
+                          (into []))
+        init (with-meta [(unfactor init-factors) init-factors]
+                        {:rest (drop *init-count* primes)})]
+    (loop [queue (sorted-set init)]
+      (let [entry (first (seq queue))
+            [n factors] entry
+            d (prod (map #(+ 1 % %) (map second factors)))
+            c (/ (inc d) 2)]
+        (if (> c 1000)
+          n
+          (recur (reduce conj (disj queue entry)
+                         (cons (add-factor entry)
+                               (inc-factors entry)))))))))
+(prn ans)
+;(repl)
