cc93017a1595db11db7471b8e791c2af44718df1
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-05-18 13:17:30 -0700

    Solved Problem 111.

--- p111.clj
+++ p111.clj
@@ -11,26 +11,31 @@
 
 (set! *warn-on-reflection* true)
 
-
 (defn primes-with-repeated-digits
-  "Returns a sequence of probable primes, each with n digits, k of which
-  are repeated."
-  [n k]
-  (binding [*prime-certainty* (* 2 (int (/ (Math/log 1.0e11)
+  "Returns a sequence of probable primes with n digits, each with the maximum number of repetitions of the digit d."
+  ([n d]
+   (loop [k (dec n)]
+     (let [ps (primes-with-repeated-digits n d k)]
+       (if (seq ps)
+         ps
+         (recur (dec k))))))
+  ([n d k]
+   (binding [*prime-certainty* (* 4 (int (/ (Math/log 1.0e11)
                                            (Math/log 2.0))))]
-    (for [repeated-digit (range 10)
-          :let [m (- n k)]
-          other-num (with-digits m)
-          :let [other-digits (digits other-num)]
-          :when (not ((set other-digits) repeated-digit))
-          :when (< (apply max (vals (frequencies other-digits))) k)
-          order (interleavings (repeat k repeated-digit) other-digits)
-          :when (pos? (first order))
-          :let [p (from-digits order)]
-          :when (prime? p)]
-      p)))
+     (let [m (- n k)]
+       (for [other-num (range (pow 10 m))
+             :let [other-digits (->> (digits other-num)
+                                     (concat (repeat m 0))
+                                     (take-last m))]
+             :when (not ((set other-digits) d))
+             :when (< (apply max (vals (frequencies other-digits))) k)
+             order (interleavings (repeat k d) other-digits)
+             :when (pos? (first order))
+             :let [p (from-digits order)]
+             :when (prime? p)]
+         p)))))
 
 (def ans
   (delay
-    nil))
+    (sum (apply concat (for [d (range 10)] (primes-with-repeated-digits 10 d))))))
 (repl)
