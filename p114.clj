ea7227cc2bfef2310cb482edd931cbbde85a17ca
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-05-25 13:35:04 -0700

    Removed unnecessary code.

--- p114.clj
+++ p114.clj
@@ -19,29 +19,4 @@
                  (for [k (range 3 n)]
                    (fill-way-count (- n k 1))))))
      
-(defn overwrite [u i v]
-  (reduce #(assoc %1 (+ i %2) (v %2))
-          u
-          (range (count v))))
-
-(defn-memo fill-ways [n]
-  (let [base (vec (repeat n 0))]
-    (cond
-      (neg? n) nil
-      (<= 0 n 2) [base]
-      :else
-         (concat (for [w (fill-ways (dec n))]
-                   (overwrite base 1 w))
-                 (for [k (range 3 n)
-                       :let [v (vec (repeat k 1))
-                             new-base (overwrite base 0 v)]
-                       w (fill-ways (- n k 1))]
-                   (overwrite new-base (+ k 1) w))
-                 [(vec (repeat n 1))]))))
-
-(def ans
-  (delay (first (for [n (iterate inc 7)
-               :when (not= (fill-way-count n)
-                           (count (fill-ways n)))]
-           n))))
 (repl)
