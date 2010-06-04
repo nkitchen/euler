39a5763208aba8a4abdf30d20b553c57c582050a
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-06-04 14:15:17 -0700

    Solved Problem 118.

--- p118.clj
+++ p118.clj
@@ -1,6 +1,7 @@
 (ns euler.p118
   (:use euler.core
         clojure.contrib.combinatorics
+        clojure.contrib.def
         clojure.contrib.pprint
         clojure.contrib.repl-ln
         clojure.contrib.repl-utils
@@ -9,12 +10,22 @@
 
 (set! *warn-on-reflection* true)
 
-(def primes (for [n (range 1 9)
-                  c (combinations (range 1 10) n)
-                  :when (or (< n 8) (some #{1} c))
-                  p (permutations c)
-                  :let [q (from-digits p)]
-                  :when (prime? q)]
-              q))
-(prn (take-last 100 primes))
-;(repl)
+(defn-memo prime-partitions
+  "Returns a sequence of all the ways that the digits can be partitioned
+  so that each partition is a prime number (preserving the order of the
+  digits."
+  [digits]
+  (if (empty? digits)
+    [nil]
+    (for [k (range 1 (inc (count digits)))
+          :let [p (from-digits (take k digits))]
+          :when (prime? p)
+          r (prime-partitions (drop k digits))]
+      (cons p r))))
+
+(defn p188 []
+  (count (distinct (map set
+                        (apply concat (for [p (permutations (range 1 10))]
+                                        (prime-partitions p)))))))
+
+(repl)
