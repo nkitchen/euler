9631df8db9772703ea3cdc0be1180f34a8b37b04
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-06-12 17:09:10 -0700

    First attempt at Problem 122.

---
+++ p122.clj
@@ -0,0 +1,27 @@
+(ns euler.p122
+  (:use euler.core
+        clojure.set
+        clojure.contrib.def
+        clojure.contrib.pprint
+        clojure.contrib.repl-ln
+        clojure.contrib.repl-utils
+        clojure.contrib.seq-utils
+        clojure.contrib.str-utils))
+
+(set! *warn-on-reflection* true)
+
+(defn-memo best-terms [k]
+  (if (= k 1)
+    [#{1}]
+    (let [ps (for [a (range 1 (inc (quot k 2)))
+                   s (best-terms a)
+                   t (best-terms (- k a))]
+               (union #{k} s t))
+          m (apply min (map count ps))]
+      (filter #(= m (count %)) ps))))
+
+(defn p122 []
+  (sum (for [k (range 1 201)]
+         (count (first (best-terms k))))))
+
+(repl)
