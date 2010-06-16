acdba65bba5cc7be7122c145cbe18a5feaab97ea
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-06-15 17:42:12 -0700

    New approach for Problem 122.

--- p122.clj
+++ p122.clj
@@ -30,11 +30,60 @@
                 ps (all-terms k m)]
             ps))])
 
+(def *max-k* 200)
+
+(defn seq-compare [[x & r :as a] [y & s :as b]]
+  (let [c (compare x y)]
+    (cond
+      (and (empty? a) (empty? b)) 0
+      (neg? c) -1
+      (zero? c) (recur r s)
+      (pos? c) 1)))
+
+(defn entry-compare [[m a] [n b]]
+  (let [c (compare m n)]
+    (cond
+      (neg? c) -1
+      (pos? c) 1
+      (zero? c) (seq-compare a b))))
+
+(defn best-terms []
+  (let [entry (fn [e] [(count e) e])]
+    (loop [queue (sorted-set-by entry-compare (entry `(1)))
+           expansions nil
+           best {1 `(1)}]
+      (cond
+        (= (count best) *max-k*)
+        best
+        (and (empty? expansions) (empty? queue))
+        best
+        (empty? expansions)
+          (let [[_ s :as e] (first queue)
+                m (first s)]
+            (recur (disj queue e)
+                   (for [x (seq s)
+                         :let [y (+ x m)]
+                         :when (<= y *max-k*)]
+                     (conj s y))
+                   best))
+        :else
+          (let [s (first expansions)
+                m (first (seq s))]
+            (prn s (count best))
+            (if (or (nil? (best m))
+                    (< (count s) (count (best m))))
+              (recur (conj queue (entry s))
+                     (next expansions)
+                     (do
+                       (print ".")
+                       (flush)
+                       (assoc best m s)))
+              (recur (conj queue (entry s))
+                     (next expansions)
+                     best)))))))
+
 (defn p122 []
-  (sum (for [k (range 1 201)]
-         (do
-           (print ".")
-           (flush)
-           (count (first (best-terms k)))))))
+  (sum (map #(dec (count %)) (vals (best-terms)))))
+
 
 (repl)
