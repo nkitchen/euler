0543253283f055ce3613eed22a8ec45fe929380d
Nathan Kitchen <nbk@eecs.berkeley.edu>
2010-06-25 13:28:19 -0700

    Solved Problem 124.

--- p124.clj
+++ p124.clj
@@ -8,7 +8,6 @@
         clojure.contrib.str-utils))
 
 (set! *warn-on-reflection* true)
-(set! *print-length* 6)
 
 (defn rad-extend [[r fs ms [p & qs]]]
   (if (= 1 (sum ms))
@@ -20,45 +19,33 @@
         :while (zero? (ms i))]
     [(* r (fs i)) fs (assoc ms i 1) next-primes]))
 
-(defn radicals
-  "Returns a sorted sequence of products of prime factors."
+(defn radical-factors
+  "Returns a sequence of factor groups, sorted by radical (i.e, their product)."
   []
   (let [f (fn f [queue]
-            (let [[r & other :as entry] (first queue)]
+            (let [[r fs ms & other :as entry] (first queue)]
               (lazy-seq
-                (cons r
+                (cons (for [[f m] (map vector fs ms)
+                            :when (pos? m)]
+                        f)
                       (f (reduce conj (disj queue entry)
                                  (concat (rad-extend entry)
                                          (rad-inc entry))))))))]
     (f (sorted-set [1 [] [] primes]
                    [2 [2] [1] (next primes)]))))
 
+(defn products [factors max]
+  (if (empty? factors)
+    [1]
+    (for [k (iterate inc 1)
+          :let [p (pow (first factors) k)]
+          :while (<= p max)
+          q (products (rest factors) (quot max p))]
+      (* p q))))
 
-(defn inc-factors [entry]
-  (let [[n fs ms] entry]
-    (for [[i f] (indexed fs)]
-      (with-meta [(* f n) fs (update-in ms [i] inc)]
-                 (meta entry)))))
-
-#_(defn radicals [max-n]
-  (let [entry1 (with-meta [1 [] []] {:rest primes})]
-    (loop [queue (sorted-set entry1)
-           radicals []]
-      (let [entry (first queue)
-            [n fs ms] entry
-            _ (do (prn n) (flush))
-            r (prod (for [[i m] (indexed ms)
-                          :when (pos? m)]
-                      (fs i)))]
-        (cond
-          (> n max-n) radicals
-          :else
-            (recur (reduce conj (disj queue entry)
-                           (concat (add-factor entry)
-                                   (inc-factors entry)))
-                   (conj radicals [n r])))))))
-
-(defn p124 [max-n k]
-  (first (nth (sort-by second (radicals max-n)) (dec k))))
+(defn p124 [max k]
+  (let [s (apply concat (for [fs (radical-factors)]
+                          (sort (products fs max))))]
+    (nth s (dec k))))
 
 (repl)
